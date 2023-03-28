97ddb798ac6ad52b75cc727d518273b6d72d8e58
Aida Ismayilova <aida@fidelis.az>
2023-03-29 00:37:26 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12) 
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'New Iphone',
+    amount: 1500,
+    date: new Date(2022, 9, 19),
+  },
+  {
+    id: 'e6',
+    title: 'Watch',
+    amount: 500,
+    date: new Date(2022, 1, 12),
+  },
+  {
+    id: 'e7',
+    title: 'Flowers',
+    amount: 20,
+    date: new Date(2021, 2, 22),
+  },
+  {
+    id: 'e8',
+    title: 'Sofa',
+    amount: 5,
+    date: new Date(2020, 7, 1),
+  },
+  {
+    id: 'e9',
+    title: 'Mirror',
+    amount: 50,
+    date: new Date(2020, 3, 16),
+  },
+  {
+    id: 'e10',
+    title: 'Car',
+    amount: 5000,
+    date: new Date(2022, 5, 10),
+  },
+  {
+    id: 'e11',
+    title: 'Coffee',
+    amount: 20,
+    date: new Date(2022, 1, 1),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
